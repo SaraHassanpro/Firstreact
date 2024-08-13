f2b0d8001f2bbc4a69894ab995e3a002e622f16e
SaraHassanpro <110007834+SaraHassanpro@users.noreply.github.com>
2024-08-13 15:17:44 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navebar from "./Home/Navebar";
+import Header from "./Home/Header";
+import About from "./Home/About";
+import Device from "./Home/Device";
+import Footer from "./Home/Footer";
+
+function App() {
+  return (
+    <div >
+      <Navebar />
+      <Header />
+      <About />
+      <Device />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
